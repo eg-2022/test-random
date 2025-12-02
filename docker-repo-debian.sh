a883e5c91ccf17aecd8ab66a2f4d1da9d4234d59
Eduardo Garay <eduardo.garay.2022@gmail.com>
2025-12-02 04:44:15 -0600

    Create docker-repo-debian.sh

---
+++ docker-repo-debian.sh
@@ -0,0 +1,7 @@
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
